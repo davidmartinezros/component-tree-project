0d0ed9dc24f56f25c893cc5de7bda208c85481bf
David Martinez Ros <davidnezan@gmail.com>
2016-11-11 18:30:55 +0100

    simplify the fields in the node class and make the up and down movement of nodes between siblings

--- src/app/node-tree/node-tree.component.ts
+++ src/app/node-tree/node-tree.component.ts
@@ -13,6 +13,8 @@ export class NodeTreeComponent implements OnInit {
 
   @Input() parent: Node;
 
+  @Input() firstNode;
+
   @Input() lastNode;
 
   constructor() { }
@@ -37,7 +39,16 @@ export class NodeTreeComponent implements OnInit {
     }
   }
 
+  getParentName(): string {
+    if(this.node.ancestors != null && this.node.ancestors.length > 0) {
+      return this.node.ancestors[this.node.ancestors.length-1].name;
+    } else {
+      return "root";
+    }
+  }
+
   loadNode() {
+    this.node.firstNode = this.firstNode;
     this.node.lastNode = this.lastNode;
     console.log("el node " + this.node.name + " te lastNode " + this.node.lastNode);
     if(this.node.ancestors == null) {
@@ -55,6 +66,101 @@ export class NodeTreeComponent implements OnInit {
       console.log("afegim parent " + this.parent.name + " amnb lastNode " + this.parent.lastNode);
     }
     console.log('el node ' + this.node.name + " te " + this.node.ancestors.length + " parents.");
+  }
+
+  upNode() {
+    console.log('upNode');
+
+    let parentTree: Node;
+
+    if(this.node.ancestors != null && this.node.ancestors.length > 0) {
+      parentTree = this.node.ancestors[this.node.ancestors.length-1];
+    }
+
+    if(parentTree != null) {
+      const nodeIndex = parentTree.childs.indexOf(this.node);
+      const siblingIndex = nodeIndex -1;
+      const siblingNode = parentTree.childs[siblingIndex];
+
+      console.log('nodeIndex:' + nodeIndex);
+      console.log('siblingIndex:' + siblingIndex);
+      console.log('this.node.ancestors.length:' + this.node.ancestors.length);
+
+      let firstNode = false;
+      let lastNode = false;
+      if(nodeIndex === 0) {
+        firstNode = true;
+      }
+      if(nodeIndex === parentTree.childs.length-1) {
+        lastNode = true;
+      }
+
+      let firstNodeSibling = false;
+      let lastNodeSibling = false;
+      if(siblingIndex === 0) {
+        firstNodeSibling = true;
+      }
+      if(siblingIndex === parentTree.childs.length-1) {
+        lastNodeSibling = true;
+      }
+      
+      this.node.lastNode = lastNodeSibling;
+      this.node.firstNode = firstNodeSibling;
+      siblingNode.lastNode = lastNode;
+      siblingNode.firstNode = firstNode;
+
+      parentTree.childs[siblingIndex] = this.node;
+      parentTree.childs[nodeIndex] = siblingNode;
+
+    }
+    
+  }
+
+  downNode() {
+    console.log('downNode');
+    
+    let parentTree: Node;
+
+    if(this.node.ancestors != null && this.node.ancestors.length > 0) {
+      parentTree = this.node.ancestors[this.node.ancestors.length-1];
+    }
+
+    if(parentTree != null) {
+      const nodeIndex = parentTree.childs.indexOf(this.node);
+      const siblingIndex = nodeIndex +1;
+      const siblingNode = parentTree.childs[siblingIndex];
+
+      console.log('nodeIndex:' + nodeIndex);
+      console.log('siblingIndex:' + siblingIndex);
+      console.log('parentTree.childs.length:' + parentTree.childs.length);
+
+      let firstNode = false;
+      let lastNode = false;
+      if(nodeIndex === 0) {
+        firstNode = true;
+      }
+      if(nodeIndex === parentTree.childs.length-1) {
+        lastNode = true;
+      }
+
+      let firstNodeSibling = false;
+      let lastNodeSibling = false;
+      if(siblingIndex === 0) {
+        firstNodeSibling = true;
+      }
+      if(siblingIndex === parentTree.childs.length-1) {
+        lastNodeSibling = true;
+      }
+      
+      this.node.lastNode = lastNodeSibling;
+      this.node.firstNode = firstNodeSibling;
+      siblingNode.lastNode = lastNode;
+      siblingNode.firstNode = firstNode;
+
+      parentTree.childs[siblingIndex] = this.node;
+      parentTree.childs[nodeIndex] = siblingNode;
+
+    }
   }
 
 }
--- src/app/node.class.ts
+++ src/app/node.class.ts
@@ -1,11 +1,13 @@
 export class Node {
     id: number;
     name: string;
+    description: string;
 
     childs: Node[];
 
     expand: boolean = false;
 
+    firstNode: boolean;
     lastNode: boolean;
 
     ancestors: Node[];
